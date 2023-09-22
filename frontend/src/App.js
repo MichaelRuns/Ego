c99bcdcbef99df879b4b4e8d1dab5bddc9f8781d
Michael Vernau <102547230+MichaelRuns@users.noreply.github.com>
2023-09-22 13:36:29 -0700

    fix installs.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-import TestComponent from './components/test_component';
+import HeaderBanner from './components/header_banner';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="text-4xl font-bold mb-4">Testing Tailwind CSS</h1>
-        <TestComponent />
-      </header>
+    <div>
+      <h1>
+        <HeaderBanner />
+      </h1>
+
     </div>
   );
 }
